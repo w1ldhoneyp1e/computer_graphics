343244cb43246a42bdddc7f6b33f3e55408ab4f2
kirill.yashmetov <k_yashmetov@mail.ru>
2026-02-06 08:34:33 +0300

    lab1-1 нарисовал букву Р

--- lab1/main.ts
+++ lab1/main.ts
@@ -68,24 +68,17 @@ function drawR() {
     if (!ctx) throw new Error('2d context not available')
 
     ctx.beginPath()
-    ctx.rect(250, 100, 25, 200)
+    ctx.rect(400, 100, 100, 100)
     ctx.fillStyle = BLACK_COLOR
     ctx.fill()
 
     ctx.beginPath()
-    ctx.moveTo(275, 175)
-    ctx.lineTo(325, 100)
-    ctx.lineTo(350, 100)
-    const yCenterOfK = 75 / 50 * 75 + 100
-    ctx.lineTo(275, yCenterOfK)
-    ctx.fillStyle = BLACK_COLOR
+    ctx.rect(425, 125, 50, 50)
+    ctx.fillStyle = WHITE_COLOR
     ctx.fill()
 
     ctx.beginPath()
-    ctx.moveTo(275, yCenterOfK - 40)
-    ctx.lineTo(350, 300)
-    ctx.lineTo(325, 300)
-    ctx.lineTo(275, 300 - (300 - yCenterOfK + 40) / 75 * 50)
+    ctx.rect(400, 100, 25, 200)
     ctx.fillStyle = BLACK_COLOR
     ctx.fill()
 }
