9f8a4e68a0dc5c0742e7579ff584a0c2922fcc99
kirill.yashmetov <k_yashmetov@mail.ru>
2026-02-12 23:43:42 +0300

    lab1-2 добавил функции для рисования цветка

---
+++ lab1/flower.ts
@@ -0,0 +1,56 @@
+import {type Point} from './types'
+
+const PETAL_RADIUS = 20
+const PETAL_HEIGHT = 5
+const PETAL_COLOR = '#ff0000' // red
+
+const STALK_HEIGHT = 100
+const STALK_WIDTH = 10
+const STALK_COLOR = '#009000' // green
+
+const CENTER_RADIUS = 50
+const CENTER_COLOR = '#ffff00' // yellow
+
+function drawPetals(ctx: CanvasRenderingContext2D, amount: number, center: Point) {
+	for (let i = 0; i < amount; i++) {
+		const angle = i * 2 * Math.PI / amount
+		drawPetal(ctx, angle, center)
+	}
+}
+
+function drawPetal(ctx: CanvasRenderingContext2D, angle: number, center: Point) {
+	const x = center.x + CENTER_RADIUS * Math.cos(angle)
+	const y = center.y + CENTER_RADIUS * Math.sin(angle)
+
+	ctx.strokeStyle = PETAL_COLOR
+	ctx.ellipse(x, y, PETAL_RADIUS, PETAL_HEIGHT, 0, 0, angle)
+	ctx.fillStyle = PETAL_COLOR
+	ctx.fill()
+}
+
+function drawStalk(ctx: CanvasRenderingContext2D, center: Point) {
+	ctx.strokeStyle = STALK_COLOR
+	ctx.beginPath()
+	ctx.moveTo(center.x, center.y)
+	ctx.lineTo(center.x, center.y + STALK_HEIGHT)
+	ctx.lineTo(center.x + STALK_WIDTH, center.y + STALK_HEIGHT)
+	ctx.lineTo(center.x + STALK_WIDTH, center.y)
+	ctx.fillStyle = STALK_COLOR
+	ctx.fill()
+}
+
+function drawCenter(ctx: CanvasRenderingContext2D, center: Point) {
+	ctx.strokeStyle = CENTER_COLOR
+	ctx.beginPath()
+	ctx.arc(center.x, center.y, CENTER_RADIUS, 0, 2 * Math.PI)
+	ctx.fillStyle = CENTER_COLOR
+	ctx.fill()
+}
+
+
+export {
+	drawPetals,
+	drawPetal,
+	drawStalk,
+	drawCenter,
+}
