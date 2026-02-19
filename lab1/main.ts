0b235bdf04e67c53dab848549bbb0dde581ae5d5
kirill.yashmetov <k_yashmetov@mail.ru>
2026-02-19 15:51:25 +0300

    lab1-3 добавил обработку формы

--- lab1/main.ts
+++ lab1/main.ts
@@ -1,4 +1,4 @@
-import {draw} from './flower'
+import {draw as drawTriangle} from './triangle'
 import {type Drawable} from './types'
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement
@@ -6,35 +6,56 @@ const ctx = canvas.getContext('2d')
 if (!ctx) {
 	throw new Error('2d context not available')
 }
+const context = ctx
 
-const SCENE: Drawable[] = [
-	{
-		type: 'flower',
-		center: {
-			x: 200,
-			y: 200,
-		},
-		petalsAmount: 10,
-		petalColor: '#ff0000',
-		innerPetals: {
-			color: '#FF4F4FEE',
-			angleOffset: Math.PI / 12,
-		},
-		draw,
-	},
-]
+let scene: Drawable[] = []
 
-function renderScene(context: CanvasRenderingContext2D, scene: Drawable[]) {
-	for (const drawableObj of scene) {
+function renderScene(renderingContext: CanvasRenderingContext2D, sceneToRender: Drawable[]) {
+	for (const drawableObj of sceneToRender) {
 		switch (drawableObj.type) {
 			case 'flower':
-				drawableObj.draw(context, drawableObj)
+				drawableObj.draw(renderingContext, drawableObj)
 				break
 			case 'letter':
-				drawableObj.draw(context, drawableObj)
+				drawableObj.draw(renderingContext, drawableObj)
+				break
+			case 'triangle':
+				drawableObj.draw(renderingContext, drawableObj)
 				break
 		}
 	}
 }
 
-renderScene(ctx, SCENE)
+function repaint() {
+	context.clearRect(0, 0, canvas.width, canvas.height)
+	renderScene(context, scene)
+}
+
+const form = document.getElementById('triangle-form') as HTMLFormElement
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	const data = new FormData(form)
+	scene = [
+		...scene,
+		{
+			type: 'triangle',
+			v0: {
+				x: Number(data.get('v0x')),
+				y: Number(data.get('v0y')),
+			},
+			v1: {
+				x: Number(data.get('v1x')),
+				y: Number(data.get('v1y')),
+			},
+			v2: {
+				x: Number(data.get('v2x')),
+				y: Number(data.get('v2y')),
+			},
+			fillColor: String(data.get('fillColor')),
+			draw: drawTriangle,
+		},
+	]
+	repaint()
+})
+
+repaint()
